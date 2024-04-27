d42482e16224856d28a65d2b3e49178e1901cc52
brandondando <dandobrandon0@gmail.com>
2024-04-27 07:46:28 +0300

    first commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+   const [isInCart, setIsInCart] = useState(false);
+   const toggleCart = () => {
+    setIsInCart(!isInCart);
+  };
+
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+    <li className={isInCart ? "in-cart" : ""}>
+    <span>{name}</span>
+    <span className="category">{category}</span>
+    <button className="add" onClick={toggleCart}>
+      {isInCart ? "Remove From Cart" : "Add to Cart"}
+    </button>
+  </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
